da761dd33523763877c2e084d5d6d15add541cb7
krist1122 <kristsalem@gmail.com>
2026-07-27 01:22:46 +0800

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -22,7 +22,7 @@ No Free 🫪 GETS!?</i></blockquote>"""
 # 2. TEMPLATE PARA SA CODM
 CODM_MESSAGE = """<blockquote expandable="true"><i><a href="https://t.me/SliderModMenuMlbb/380">Latest Update:
 Call of Duty v1.6.56
-Injector v2.7.6</a>
+Injector v2.7.7</a>
 
 NEED KEY LOGIN ??? :
 For inquiries and availment, send a direct message ~ <a href="https://t.me/phia_maganda">𝑷𝒉𝒊𝒂 𝑭𝒆𝒍𝒊𝒄𝒊𝒂</a>
