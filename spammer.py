13daac3fedb1bac67cab55db421c49cb5c8ca8dc
krist1122 <kristsalem@gmail.com>
2026-08-08 13:33:36 +0800

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -20,13 +20,13 @@ No Free 🫪 GETS!?</i></blockquote>"""
 
 # 2. TEMPLATE PARA SA CODM
 CODM_MESSAGE = """<blockquote expandable="true"><i><a href="https://t.me/SliderModMenuMlbb/380">Latest Update: Call of Duty v1.6.56
-Garena Injector v2.7.8
+Garena Injector v2.7.9
 Global Injector v1.2.5</a>
 
 NEED KEY LOGIN ??? :
 For inquiries and availment, send a direct message ~ <a href="https://t.me/phia_maganda">𝑷𝒉𝒊𝒂 𝑭𝒆𝒍𝒊𝒄𝒊𝒂</a>
 
-<a href="https://slider-info-slidermod-register-deviceid.onrender.com/free">Try Free Key Here:</a></i></blockquote>"""
+<a href="https://slidermodmenucodm-registerdeviceid-garena.onrender.com/free">Try Trial Key Here:</a></i></blockquote>"""
 
 # 3. BAGONG TEMPLATE PARA SA PROMO (May expandable quote at bagong link)
 PROMO_MESSAGE = """<blockquote expandable="true"><i><a href="https://t.me/SliderModMenuMlbb/8279">Promo Available :
